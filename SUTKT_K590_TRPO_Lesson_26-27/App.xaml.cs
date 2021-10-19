f45b8a981d0bebee773528e5975c06314f6bb709
Николай Никоноров <nnv@nikolay.moe>
2021-10-19 09:23:12 +0300

    init commit

---
+++ SUTKT_K590_TRPO_Lesson_26-27/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SUTKT_K590_TRPO_Lesson_26_27
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ SUTKT_K590_TRPO_Lesson_26-27/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SUTKT_K590_TRPO_Lesson_26_27
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
